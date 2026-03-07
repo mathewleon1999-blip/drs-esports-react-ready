3f4424d189def8e0cc11a824fc637867f75be6cd
Leon <mathewleon1999@gmail.com>
2026-03-07 17:28:56 +0400

    fix: add BrowserRouter to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -61,10 +62,12 @@ const initApp = async () => {
   }
 };
 
-// Render app
+// Render app with BrowserRouter
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
